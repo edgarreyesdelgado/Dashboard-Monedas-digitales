8e21ea2f8c928c0a782d1992e4f3891985051397
edgarreyesdelgado <reitawar@gmail.com>
2023-06-30 13:17:03 -0600

    primer commit

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const fetch = (...urls) => {
+    const promises = urls.map(url => fetch(url).then(response => response.json()))
+    return Promise.all(promises)
+}
+
+const getDataColors = opacity => {
+    const colors = ['#7448c2', '#21c0d7', '#d99e2b', '#cd3a81', '#9c99cc', '#e14eca', '#ffffff', '#ff0000', '#d6ff00', '#0038ff']
+    return colors.map(color => opacity ? `${color + opacity}` : color)
+}
+
---
+++ index.js
@@ -0,0 +1,121 @@
+
+Chart.defaults.color = '#fff'
+Chart.defaults.borderColor = '#444'
+
+const getCurrentprice= async() =>{
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    const datos = await response.json()
+    console.log(datos)
+    getChartCurrentprice(datos)
+}
+
+getCurrentprice()
+
+const getChartCurrentprice = (datos) =>{
+    const chart = document.getElementById('myChart1')
+
+const labes = datos.map((coin) => coin.name)
+const Currentprice = datos.map((coin)=> coin.current_price
+)
+
+    new Chart(chart, {
+        type: 'bar',
+        data: {
+          labels: labes,
+          datasets: [{
+            label: 'Precio Actual',
+            data: Currentprice,
+            borderWidth: 1,
+            borderColor: getDataColors(),
+               backgroundColor: getDataColors(20)
+                         
+            
+            
+          }]
+        },
+        options: {
+          scales: {
+           
+            x: {
+              grid: {
+                offset: true
+              }
+            }
+          }
+        }
+    });
+}
+
+const getBitcoin = async() =>{
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const datos = await response.json()
+    console.log(datos)
+    getChartBitcoin(datos)
+}
+
+getBitcoin()
+
+ const getChartBitcoin = (datos) =>{
+     const chart = document.getElementById('myChart2')
+
+     const labes = datos.map((coin) => coin.name)
+     const marketCap = datos.map((coin)=> coin.market_cap)
+
+     new Chart(chart, {
+         type: 'bar',
+         data: {
+           labels: labes,
+           datasets: [{
+             label: 'Valor de Mercado',
+             data: marketCap,
+             borderWidth: 1,
+             borderColor: getDataColors(),
+               backgroundColor: getDataColors(20)
+           }]
+         },
+         options: {
+           scales: {
+             y: {
+               beginAtZero: true
+             }
+           }
+         }
+     });
+ }
+
+ const getPriceChange = async() =>{
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const datos = await response.json()
+    console.log(datos)
+    getChartPriceChange(datos)
+}
+
+getPriceChange()
+
+ const getChartPriceChange = (datos) =>{
+     const chart = document.getElementById('myChart3')
+
+     const labes = datos.map((coin) => coin.name)
+     const PriceChange = datos.map((coin)=> coin.price_change_percentage_24h)
+
+     new Chart(chart, {
+         type: 'bar',
+         data: {
+           labels: labes,
+           datasets: [{
+             label: 'Cambio porcentual 24h',
+             data: PriceChange,
+             borderWidth: 1,
+             borderColor: getDataColors(),
+               backgroundColor: getDataColors(20)
+           }]
+         },
+         options: {
+           scales: {
+             y: {
+               beginAtZero: true
+             }
+           }
+         }
+     });
+ }
